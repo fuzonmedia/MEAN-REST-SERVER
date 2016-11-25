73bd529b363b828c88c7e77b533d47bf5f8561d2
Niladri Dey <niladridey933@gmail.com>
2016-11-25 16:00:43 +0530

    CORS Fix  Access-Control-Allow-Headers

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(passport.initialize());
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers, Authorization');
   next();
 });
 
